5e4c14caa85ce10c4a0ee4c29c4f891c8180f973
Ninad More <ninadyogeshmore@gmail.com>
2023-11-22 21:09:31 +0530

    quartrly added

--- lib/screens/currency_exchange_page.dart
+++ lib/screens/currency_exchange_page.dart
@@ -21,6 +21,7 @@ class _CurrencyExchangePageState extends State<CurrencyExchangePage> {
   String selectedCurrency2 = 'INR';
   String selectedDuration = 'Yearly';
   int selectedYear = 2022;
+  int selectedQuarter = 1;
 
   late List<String> currencyColumns = [];
   late List<List<dynamic>> csvData = [];
@@ -34,6 +35,21 @@ class _CurrencyExchangePageState extends State<CurrencyExchangePage> {
   Future<void> loadCSVData(int selectedYear) async {
     String csvString = await rootBundle
         .loadString('year_csvs/Exchange_Rate_Report_$selectedYear.csv');
+
+    List<List<dynamic>> parsedCsv =
+        const CsvToListConverter().convert(csvString);
+    currencyColumns = parsedCsv[0].skip(1).cast<String>().toList();
+
+    setState(() {
+      csvData = parsedCsv;
+    });
+  }
+
+  Future<void> loadQuarterlyCSVData(
+      int selectedYear, int selectedQuarter) async {
+    String csvString = await rootBundle.loadString(
+      'quarter_csvs/${selectedYear}_Quarter$selectedQuarter.csv',
+    );
 
     List<List<dynamic>> parsedCsv =
         const CsvToListConverter().convert(csvString);
@@ -66,9 +82,40 @@ class _CurrencyExchangePageState extends State<CurrencyExchangePage> {
     return chartData;
   }
 
+  List<ChartSampleData> getQuarterlyChartData(
+    String currency1,
+    String currency2,
+    int selectedYear,
+    int selectedQuarter,
+  ) {
+    List<ChartSampleData> chartData = [];
+
+    int indexCurrency1 = currencyColumns.indexOf(currency1) + 1;
+    int indexCurrency2 = currencyColumns.indexOf(currency2) + 1;
+
+    for (int i = 1; i < csvData.length; i++) {
+      DateTime? date = DateFormat('dd-MMM-yy').parse(csvData[i][0].toString());
+      double valueCurrency1 =
+          double.tryParse(csvData[i][indexCurrency1].toString()) ?? 0.0;
+      double valueCurrency2 =
+          double.tryParse(csvData[i][indexCurrency2].toString()) ?? 0.0;
+
+      if (valueCurrency1 != 0.0 && valueCurrency2 != 0.0) {
+        double ratio = valueCurrency2 / valueCurrency1;
+        chartData.add(ChartSampleData(date, ratio));
+      }
+    }
+
+    return chartData;
+  }
+
   Widget buildChart() {
     if (csvData.isEmpty) {
-      return const Center(child: CircularProgressIndicator());
+      return const Center(
+        child: Text(
+          'No data available',
+        ),
+      );
     } else {
       return SfCartesianChart(
         primaryXAxis: DateTimeAxis(
@@ -93,6 +140,57 @@ class _CurrencyExchangePageState extends State<CurrencyExchangePage> {
         ],
         plotAreaBackgroundColor: Colors.white,
         plotAreaBorderColor: Colors.grey,
+      );
+    }
+  }
+
+  Widget buildQuarterlyDropdown() {
+    if (selectedDuration != 'Quarterly') {
+      return const SizedBox.shrink();
+    } else {
+      return Row(
+        children: [
+          DropdownButton<int>(
+            value: selectedYear,
+            onChanged: (int? newValue) {
+              setState(() {
+                selectedYear = newValue ?? 2022;
+                // Load available quarters based on the selected year
+                // Update available quarters for that year
+              });
+            },
+            // Dropdown items for years
+            items: List<DropdownMenuItem<int>>.generate(
+              11,
+              (index) {
+                return DropdownMenuItem<int>(
+                  value: 2012 + index,
+                  child: Text((2012 + index).toString()),
+                );
+              },
+            ),
+          ),
+          const SizedBox(width: 20),
+          DropdownButton<int>(
+            value: selectedQuarter,
+            onChanged: (int? newValue) {
+              setState(() {
+                selectedQuarter = newValue ?? 1; // Update selected quarter
+                loadQuarterlyCSVData(selectedYear, selectedQuarter);
+              });
+            },
+            // Dropdown items for quarters
+            items: List<DropdownMenuItem<int>>.generate(
+              4,
+              (index) {
+                return DropdownMenuItem<int>(
+                  value: index + 1,
+                  child: Text('Quarter ${index + 1}'),
+                );
+              },
+            ),
+          ),
+        ],
       );
     }
   }
@@ -158,6 +256,7 @@ class _CurrencyExchangePageState extends State<CurrencyExchangePage> {
                   ),
                 ),
               ),
+            if (selectedDuration == 'Quarterly') buildQuarterlyDropdown(),
             const SizedBox(height: 20),
             Expanded(
               child: buildChart(),
